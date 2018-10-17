fbfe0175b9919db67ad59aa2ce4e5652cc05d82e
wippyzulkarnain <wippy.zulkarnain@gmail.com>
2018-10-17 17:37:10 +0700

    remove

--- index.js
+++ index.js
@@ -1,43 +1,65 @@
-let book=[{
-    fname : "Jack" , 
-    lname : "Bauer",
-    email : "Jack@gmail.com",
-    address : "CTU"}]
+let book = [
+  {
+    fname: "Jack",
+    lname: "Bauer",
+    email: "Jack@gmail.com",
+    address: "CTU"
+  },
+  {
+    fname: "John",
+    lname: "Mcclane",
+    email: "John@gmail.com",
+    address: "New York"
+  },
+  {
+    fname: "Tom",
+    lname: "Clancy",
+    email: "Tom@gmail.com",
+    address: "Washington"
+  }
+];
 
-    function add(fname,lname,email,address){ 
-    book.push(
-    {fname : fname , 
-    lname : lname,
-    email : email,
-    address : address})
+function add(fname, lname, email, address) {
+  book.push({
+    fname: fname,
+    lname: lname,
+    email: email,
+    address: address
+  });
 }
-function display(){
-book.forEach(function (entry){
-    document.write(`First name : ${entry.fname} </br> Last name : ${entry.lname} </br> Email : ${entry.email} </br> Address : ${entry.address} </br></br>`)
-})}
-
-function search(keyword){
-   for (i=0 ; i < book.length ; i++){
-       if (keyword === book[i].fname)
-       {
-           console.log(book[i])
-       }
-       else if (keyword === book[i].lname)
-       {
-           console.log(book[i])
-       }
-       else if (keyword === book[i].email)
-       {
-           console.log(book[i])
-       }
-       else if (keyword === book[i].address)
-       {
-           console.log(book[i])
-       }
-       else{
-           console.log("No Result")
-       }
-   }
-    
+function display() {
+  book.forEach(function(entry) {
+    document.write(
+      `First name : ${entry.fname} </br> Last name : ${
+        entry.lname
+      } </br> Email : ${entry.email} </br> Address : ${
+        entry.address
+      } </br></br>`
+    );
+  });
 }
 
+function search(keyword) {
+  for (i = 0; i < book.length; i++) {
+    if (keyword === book[i].fname) {
+      console.log(book[i]);
+    } else if (keyword === book[i].lname) {
+      console.log(book[i]);
+    } else if (keyword === book[i].email) {
+      console.log(book[i]);
+    } else if (keyword === book[i].address) {
+      console.log(book[i]);
+    } else {
+      console.log("No Result");
+    }
+  }
+}
+
+function remove(keyword){
+    for (i = 0; i < book.length; i++) {
+        if (keyword === book[i].fname) {
+            book.splice(i,1)
+            console.log("success !")    
+}
+    }
+}
