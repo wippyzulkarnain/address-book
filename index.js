a03d5cdc4075b4094c697eadb033c8cadfbf7828
wippyzulkarnain <wippy.zulkarnain@gmail.com>
2018-10-19 17:10:45 +0700

    updated with HTML

--- index.js
+++ index.js
@@ -39,55 +39,55 @@ function display() {
   });
 }
 
-function search(keyword) {
+function remove(keyword) {
   for (i = 0; i < book.length; i++) {
-    if (keyword.toLowerCase() === book[i].fname.toLowerCase()) {
-      console.log(book[i]);
-    } else if (keyword.toLowerCase() === book[i].lname.toLowerCase()) {
-      console.log(book[i]);
-    } else if (keyword.toLowerCase() === book[i].email.toLowerCase()) {
-      console.log(book[i]);
-    } else if (keyword.toLowerCase() === book[i].address.toLowerCase()) {
-      console.log(book[i]);
+    if (keyword === book[i].fname) {
+      book.splice(i, 1);
+      console.log("success !");
     }
   }
 }
 
-function remove(keyword){
-    for (i = 0; i < book.length; i++) {
-        if (keyword === book[i].fname) {
-            book.splice(i,1)
-            console.log("success !")    
+function modify(index, key, keyword) {
+  book[index][key] = keyword;
 }
+
+function search2(key, value) {
+  let result = book.filter(function(entry) {
+    return entry[key] === value;
+  });
+  console.log(result);
+}
+
+function search3(key, value) {
+  let result = book.filter(function(entry) {
+    if (typeof entry[key] === "string") {
+      return entry[key].toLowerCase().includes(value.toLowerCase());
+    } else {
+      return entry[key] === value;
     }
+  });
+  console.log(result);
 }
+var result = document.getElementById("result");
+var ul = document.createElement("ul");
 
-function modify(index,key,keyword){
-    book[index][key]=keyword;
+const displaybutton = document.getElementById("displaybutton");
+
+displaybutton.addEventListener("click", display);
+function display() {
+  ul.innerHTML = "";
+  for (i = 0; i < book.length; i++) {
+    console.log(book[i].fname);
+
+    var li = document.createElement("li");
+    li.textContent = `Index : ${i} | First Name : ${book[i].fname} | Last Name : ${book[i].lname} | Address:${
+      book[i].address
+    } | Email:${book[i].email} `;
+    ul.appendChild(li);
+  }
+  result.appendChild(ul);
 }
-
-function search2(key, value){
-  let result = book.filter(
-    function (entry){
-      return entry[key] === value
-    }
-  )
-  console.log(result)
-}
-
-function search3(key, value){
-  let result = book.filter(
-    function (entry){
-      if (typeof entry[key] === "string"){
-        return entry[key].toLowerCase().includes(value.toLowerCase())
-      }
-      else {
-      return entry[key] === value
-    }}
-  )
-  console.log(result)
-}
-
 // if (b === "fname") {
 //   book[a].fname = keyword
 //   console.log("success !")
@@ -107,3 +107,63 @@ function search3(key, value){
 //               else{
 //                   console.log("fail")
 //               }
+
+//
+
+const submitbutton = document.getElementById("submitbutton");
+// const fname = document.getElementById('fname')
+submitbutton.addEventListener("click", submit);
+
+function submit() {
+  var temp = {};
+  temp["fname"] = document.getElementById("fname").value;
+  temp["lname"] = document.getElementById("lname").value;
+  temp["email"] = document.getElementById("email").value;
+  temp["address"] = document.getElementById("address").value;
+  book.push(temp);
+}
+
+const searchbutton = document.getElementById("searchbutton");
+searchbutton.addEventListener("click", search);
+
+function search() {
+  ul.innerHTML = "";
+
+  for (i = 0; i < book.length; i++) {
+    var li = document.createElement("li");
+    if (
+      document.getElementById("search").value.toLowerCase() ===
+      book[i].fname.toLowerCase()
+    ) {
+      li.textContent = `Name : ${book[i].fname} ${book[i].lname} Address:${
+        book[i].address
+      } Email:${book[i].email} `;
+      ul.appendChild(li);
+    } else if (
+      document.getElementById("search").value.toLowerCase() ===
+      book[i].lname.toLowerCase()
+    ) {
+      li.textContent = `Name : ${book[i].fname} ${book[i].lname} Address:${
+        book[i].address
+      } Email:${book[i].email} `;
+      ul.appendChild(li);
+    } else if (
+      document.getElementById("search").value.toLowerCase() ===
+      book[i].email.toLowerCase()
+    ) {
+      li.textContent = `Name : ${book[i].fname} ${book[i].lname} Address:${
+        book[i].address
+      } Email:${book[i].email} `;
+      ul.appendChild(li);
+    } else if (
+      document.getElementById("search").value.toLowerCase() ===
+      book[i].address.toLowerCase()
+    ) {
+      li.textContent = `Name : ${book[i].fname} ${book[i].lname} Address:${
+        book[i].address
+      } Email:${book[i].email} `;
+      ul.appendChild(li);
+    }
+  }
+  result.appendChild(ul);
+}
